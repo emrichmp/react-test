520ae82bb129f4fd632829d69e75fa97a1f1d550
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-22 16:37:46 -0700

    experimenting w/ Python

--- pythontest/.vscode/hello.py
+++ pythontest/.vscode/hello.py
@@ -1,3 +1,11 @@
-msg = 10*10
+# msg = 10*10
 
-print(msg)
+# print(msg)
+
+fruits = ["apple", "strawberry", "banana"]
+
+for x in fruits:
+    if x == "strawberry":
+        print("Breaking the loop")
+        break
+    print(x)
