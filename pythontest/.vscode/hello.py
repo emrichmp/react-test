a15f52b5ada946f515042b8a24fae97a9db22779
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-26 11:23:23 -0700

    messed around with loops in python, took notes

--- pythontest/.vscode/hello.py
+++ pythontest/.vscode/hello.py
@@ -4,8 +4,35 @@
 
 fruits = ["apple", "strawberry", "banana"]
 
-for x in fruits:
-    if x == "strawberry":
-        print("Breaking the loop")
-        break
-    print(x)
+
+#simple for loop with break on a case
+# for x in fruits:
+#     if x == "strawberry":
+#         print("Breaking the loop")
+#         break
+#     print(x)
+
+#basically skips on a case
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":3
+#     continue
+#   print(x)
+
+# for x in range(6): #returns a sequence of numbers starting from 0 to 1 less the num (0-5)
+#   print(x)
+
+#range(x, y ,z)
+# x = start
+# y = end
+# z = incriment
+
+# else syntax is just 'else:'
+# for x in range(6):
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+# for loops cant be empty but u can do a pass to not get an error
+for x in range(3):
+    pass
